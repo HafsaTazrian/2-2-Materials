0e8b64dc9bf9b2b33c8f37c3b8a82be226d037b2
HafsaTazrian <hafsa.tazrian@gmail.com>
2024-10-30 08:02:13 +0600

    cse 2200

---
+++ CSE-2200/java/BasicJava/src/BasicJava/Product.java
@@ -0,0 +1,21 @@
+package BasicJava;
+
+public class Product {
+    public static void main(String[] args){
+        int id;
+        String title, price,description,category;
+        id = 101;
+        title = "iphone15";
+        price = "1895 euros";
+        description= "perfect product with best image quality";
+        category = "phone";
+
+
+        System.out.println("id: " +id);
+        System.out.println("title: "+title);
+        System.out.println("price: "+price);
+        System.out.println("description: "+description);
+        System.out.println("category: "+category);
+    }
+
+}
---
+++ CSE-2200/java/BasicJava/src/BasicJava/hello.java
@@ -0,0 +1,8 @@
+package BasicJava;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("YOOHOO!");
+        System.out.println(" single quote\r");
+    }
+ }
